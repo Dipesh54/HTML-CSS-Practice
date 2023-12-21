596c2305cb47b5c378963ff7d4c740493c71a583
Dipesh Kumar <dipesh2002kumar@gmail.com>
2023-12-21 16:15:31 +0530

    HTML CSS learnig files

---
+++ functioninjs.js
@@ -0,0 +1,20 @@
+function myfunction(string){
+    console.log(string);
+}
+let string= 'Dipesh is a good guy';
+let string1='he is a web developer';
+let string2='he always working hard';
+let string3='now he become great guy';
+function sum(a,b,c){
+
+    console.log(a+b+c);
+
+
+}
+
+sum(2,3,4);
+sum(45,54,67);
+myfunction(string);
+myfunction(string1);
+myfunction(string2);
+myfunction(string3);
